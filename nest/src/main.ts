b59546b13b42df00782ee985888cb78d6d63dbec
lyandeveloperm <tutors3d@gmail.com>
2020-09-30 17:16:15 -0300

    Nest read the queue

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import RabbitMQServer from './rabbitmq-server';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const server = new RabbitMQServer('amqp://localhost:5672');
+  await server.start();
+  await server.consume('nest', message =>
+    console.log(message.content.toString()),
+  );
 }
 bootstrap();
---
+++ nest/src/rabbitmq-server.ts
@@ -0,0 +1,24 @@
+import { Connection, Channel, connect, Message } from 'amqplib';
+
+export default class RabbitMQServer {
+  private conn: Connection;
+  private channel: Channel;
+
+  constructor(private uri: string) {}
+
+  async start(): Promise<void> {
+    this.conn = await connect(this.uri);
+    this.channel = await this.conn.createChannel();
+  }
+
+  async publishQueue(queue: string, message: string) {
+    return this.channel.sendToQueue(queue, Buffer.from(message));
+  }
+
+  async consume(queue: string, callback: (message: Message) => void) {
+    return this.channel.consume(queue, (message) => {
+      callback(message);
+      this.channel.ack(message);
+    });
+  }
+}
